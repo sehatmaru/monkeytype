c920d6c1d17b7e48cb9251abb3361f196e8823c8
sehatmaru <sehat.samosir@adapundi.com>
2024-04-23 18:07:17 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faA, faAt, faBell, faCircleDollarToSlot, faClock, faCode, faCodeBranch, faCrown, faEnvelope, faFileContract, faGear, faGlobe, faGlobeAmericas, faHashtag, faInfo, faKeyboard, faLock, faMountain, faPalette, faPencil, faQuoteLeft, faRotateRight, faScrewdriverWrench, faShield, faWrench } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monkeytype';
+
+  faGear = faGear
+  faInfo = faInfo
+  faCrown = faCrown
+  faKeyboard = faKeyboard
+  faUser = faUser
+  faBell = faBell
+  faHashtag = faHashtag
+  faAt = faAt
+  faClock = faClock
+  faA = faA
+  faQuote = faQuoteLeft
+  faWrench = faWrench
+  faScrewdriverWrench = faScrewdriverWrench
+  faMountain = faMountain
+  faEnvelope = faEnvelope
+  faCircleDollarToSlot = faCircleDollarToSlot
+  faCode = faCode
+  faDiscord = faDiscord
+  faTwitter = faTwitter
+  faFileContract = faFileContract
+  faShield = faShield
+  faLock = faLock
+  faPalette = faPalette
+  faCodeBranch = faCodeBranch
+  faGlobeAmericas = faGlobeAmericas
+  faRotateRight = faRotateRight
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
+// import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    CommonModule,
+    // MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
